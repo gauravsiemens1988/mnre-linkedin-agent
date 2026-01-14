6a5f7aacef9b6ea9907673aeb4473cf8cdf02000
gauravsiemens1988 <gaurav.siemens1988@gmail.com>
2026-01-14 19:14:28 +0530

    Create jakson_layer.py

---
+++ approval/jakson_layer.py
@@ -0,0 +1,6 @@
+def add_company_pov(content):
+    return content + """
+
+At Jakson Green Infinity Pvt Ltd Noida, we see this as a critical step toward building
+a reliable, scalable green hydrogen and electrolyser ecosystem in India.
+"""
