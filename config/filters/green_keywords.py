1662332f41ee0b05af2fb8e5cfecc8c5a4217280
gauravsiemens1988 <gaurav.siemens1988@gmail.com>
2026-01-14 19:01:07 +0530

    Create green_keywords.py

---
+++ config/filters/green_keywords.py
@@ -0,0 +1,34 @@
+KEYWORDS = {
+    "hydrogen": [
+        "green hydrogen",
+        "electrolyser",
+        "alkaline",
+        "pem",
+        "hydrogen mobility"
+    ],
+    "green_ammonia": [
+        "green ammonia",
+        "ammonia synthesis",
+        "fertilizer decarbonization"
+    ],
+    "solar": [
+        "solar pv",
+        "bifacial",
+        "inverter",
+        "module manufacturing"
+    ],
+    "policy": [
+        "MNRE",
+        "SECI",
+        "tender",
+        "guidelines",
+        "policy",
+        "PLI scheme"
+    ],
+    "events": [
+        "expo",
+        "summit",
+        "exhibition",
+        "conference"
+    ]
+}
